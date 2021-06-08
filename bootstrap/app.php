a049306da5d5ed0c8ed4f735f2133360f5242674
Yohan González <yohan.gonzalez.almaguer@gmail.com>
2021-06-07 21:06:34 -0400

    Setup for Auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\CheckValidUuid;
+use Flipbox\LumenGenerator\LumenGeneratorServiceProvider;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -97,6 +98,7 @@ $app->routeMiddleware([
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(LumenGeneratorServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'email' => $this->faker->unique()->email,
+            'password' => app('hash')->make('1234567890')
         ];
     }
 }
